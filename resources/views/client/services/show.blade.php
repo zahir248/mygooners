146b3285e1f24d64ffd230e966c73e956ff7d3a7
Muhammad Zahiruddin <muhdzahir248@gmail.com>
2025-08-18 11:37:47 +0800

    update

--- resources/views/client/services/show.blade.php
+++ resources/views/client/services/show.blade.php
@@ -374,12 +374,7 @@
                                 Penyedia Disahkan
                             </div>
                         @endif
-                        <div class="flex items-center text-sm text-gray-500">
-                            <svg class="w-4 h-4 mr-1 text-yellow-400" fill="currentColor" viewBox="0 0 20 20">
-                                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                            </svg>
-                            <span>{{ number_format($service->user->trust_score, 1) }} rating</span>
-                        </div>
+
                         <p class="text-sm text-gray-600 mt-2">{{ $service->user->location }}</p>
                     </div>
                 </div>
@@ -475,9 +470,6 @@
                     <p>• Gunakan sistem mesej platform dahulu</p>
                     <p>• Laporkan aktiviti yang mencurigakan</p>
                 </div>
-                <button class="w-full mt-4 text-red-600 hover:text-red-700 text-sm font-medium">
-                    Laporkan Penyenaraian Ini
-                </button>
             </div>
         </div>
     </div>
